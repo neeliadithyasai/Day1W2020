5da2d5333fba8afccc624f3eef27673050d91b13
macstudent <neeliadithyasai@gmail.com>
2020-01-22 18:50:02 -0500

    day 2 datatypes conversion

--- src/main/java/com/lambton/FirstExample.java
+++ src/main/java/com/lambton/FirstExample.java
@@ -12,7 +12,56 @@ public class FirstExample
         {
             System.out.println(" adithya");
 
-            //sample commit =1
+            //day 2 code starts here
+
+            int a = 100;
+            int b;
+
+            b=200;
+
+            int c = a+b;
+
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+
+            System.out.println("sum = "+(a+b));
+
+            short x = 1000;
+            System.out.println("short max = " + Short.MAX_VALUE);
+            System.out.println("short max = " + Short.MIN_VALUE);
+
+            long y = 214748647;
+            System.out.println(y);
+
+            float f1= 1000.50F;
+            double f2=1000.40D;
+            System.out.println("f1 = " +f1);
+            System.out.println("f2 = " + f2);
+
+
+            int z= (int) (10.0f/3);
+            System.out.println(z);
+            float k=10.0f/3;
+            System.out.println(k);
+            double kk=10/3d;
+            System.out.println(kk);
+            float k2=(float)10/3;
+            System.out.println(k2);
+
+
+            double x1=1_00_00.5_0;
+            System.out.println(x1);
+
+
+
+
+
+
+
+
+
+
         }
     }
 

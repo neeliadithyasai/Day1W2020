14bda1424af0612fb05058d095577d167517d264
MacStudent <macstudent@128-IMAC24.local>
2020-01-21 20:51:11 -0500

    [Adithya]Initial commit

---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,17 @@
+package com.lambton;
+
+import java.util.Arrays;
+
+public class FirstExample
+{
+    public static void main(String[] args)
+    {
+        System.out.println("welcome to java program");
+        System.out.println("");
+        for( int i=1 ;i<=10;i++)
+        {
+            System.out.println(" adithya");
+        }
+    }
+
+}
